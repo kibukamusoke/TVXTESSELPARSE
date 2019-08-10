c291291cd3b5abe2941aae895fc6d6637ccdd9c2
Trevor Suna <trevorsuna@gmail.com>
2019-08-10 20:55:50 +0800

    -- initial commit

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/', function (req, res) {
 });
 
 /////////// portal /////////////////////////////////////////
-app.use(express.static(__dirname + '/public/admin'));
+app.use(express.static(__dirname + '/public'));
 let portal = function (req, res, next) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 };
